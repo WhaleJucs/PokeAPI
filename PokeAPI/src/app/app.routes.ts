bebeafcc6092e8e7ad516bb9b7077395de75e7fc
Joao <57973684+WhaleJucs@users.noreply.github.com>
2025-06-20 16:27:12 -0300

    Configuração da rota HTTP

--- PokeAPI/src/app/app.routes.ts
+++ PokeAPI/src/app/app.routes.ts
@@ -3,11 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
+    // Usa loadComponent para carregar a HomePage de forma "lazy" (sob demanda)
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    // Nova rota para a tela de detalhes do Pokémon
+    // ':name' é um parâmetro que pegaremos na URL (será o nome do Pokémon)
+    path: 'pokemon-details/:name',
+    // Usa loadComponent para carregar a PokemonDetailsPage de forma "lazy"
+    loadComponent: () => import('./pages/pokemon-details/pokemon-details.page').then((m) => m.PokemonDetailsPage),
   },
 ];
